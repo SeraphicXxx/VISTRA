1f92a04efd659df27e61b357549b6ac95487aa85
Kenji <kenjichua590@gmail.com>
2026-09-03 08:37:40 +0800

    Merge remote-tracking branch 'origin/Kenji-Branch' into Kenji-Branch

--- frontend/src/context/PatientContext.tsx
+++ frontend/src/context/PatientContext.tsx
@@ -81,7 +81,7 @@ export function PatientProvider({
             refreshPatients: loadPatients,
     }}
     >
-        {children}
+            {children}
         </PatientContext.Provider>
     );
 }
